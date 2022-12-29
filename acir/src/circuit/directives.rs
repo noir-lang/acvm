c051d03e6aaeb8194d113c2dc04e8329c4ac6ad6
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2022-12-29 23:48:51 +0000

    add `read` , `write` methods for directives

--- acir/src/circuit/directives.rs
+++ acir/src/circuit/directives.rs
@@ -1,4 +1,9 @@
-use crate::native_types::{Expression, Witness};
+use std::io::{Read, Write};
+
+use crate::{
+    native_types::{Expression, Witness},
+    serialisation::{read_n, read_u16, read_u32, write_bytes, write_u16, write_u32},
+};
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
@@ -18,6 +23,7 @@ pub enum Directive {
         b: Expression,
         q: Witness,
         r: Witness,
+        // TODO: why is this boxed?
         predicate: Option<Box<Expression>>,
     },
 
@@ -30,6 +36,7 @@ pub enum Directive {
     },
 
     //Computes the highest bit b of a: a = b*2^(bit_size-1) + r, where a<2^bit_size, b is 0 or 1 and r<2^(bit_size-1)
+    // TODO : change to OddRange
     Oddrange {
         a: Witness,
         b: Witness,
@@ -61,6 +68,158 @@ impl Directive {
             Directive::Oddrange { .. } => "odd_range",
             Directive::ToBits { .. } => "to_bits",
             Directive::ToBytes { .. } => "to_bytes",
+        }
+    }
+    fn to_u16(&self) -> u16 {
+        match self {
+            Directive::Invert { .. } => 0,
+            Directive::Quotient { .. } => 1,
+            Directive::Truncate { .. } => 2,
+            Directive::Oddrange { .. } => 3,
+            Directive::ToBits { .. } => 4,
+            Directive::ToBytes { .. } => 5,
+        }
+    }
+
+    pub fn write<W: Write>(&self, mut writer: W) -> std::io::Result<()> {
+        write_u16(&mut writer, self.to_u16())?;
+        match self {
+            Directive::Invert { x, result } => {
+                write_u32(&mut writer, x.witness_index())?;
+                write_u32(&mut writer, result.witness_index())?;
+            }
+            Directive::Quotient {
+                a,
+                b,
+                q,
+                r,
+                predicate,
+            } => {
+                a.write(&mut writer)?;
+                b.write(&mut writer)?;
+                write_u32(&mut writer, q.witness_index())?;
+                write_u32(&mut writer, r.witness_index())?;
+
+                let predicate_is_some = vec![predicate.is_some() as u8];
+                write_bytes(&mut writer, &predicate_is_some)?;
+
+                if let Some(pred) = predicate {
+                    pred.write(&mut writer)?;
+                }
+            }
+            Directive::Truncate { a, b, c, bit_size } => {
+                write_u32(&mut writer, a.witness_index())?;
+                write_u32(&mut writer, b.witness_index())?;
+                write_u32(&mut writer, c.witness_index())?;
+                write_u32(&mut writer, *bit_size)?;
+            }
+            Directive::Oddrange { a, b, r, bit_size } => {
+                write_u32(&mut writer, a.witness_index())?;
+                write_u32(&mut writer, b.witness_index())?;
+                write_u32(&mut writer, r.witness_index())?;
+                write_u32(&mut writer, *bit_size)?;
+            }
+            Directive::ToBits { a, b, bit_size } => {
+                a.write(&mut writer)?;
+
+                // The length of the bit vector is the same as the bit_size
+                // TODO: can we omit the bit_size altogether then?
+                write_u32(&mut writer, b.len() as u32)?;
+                for bit in b {
+                    write_u32(&mut writer, bit.witness_index())?;
+                }
+
+                write_u32(&mut writer, *bit_size)?;
+            }
+            Directive::ToBytes { a, b, byte_size } => {
+                a.write(&mut writer)?;
+
+                // TODO: can we omit the byte_size altogether?
+                // TODO see comment on ToBits about inferring this
+                // TODO from the size of the vector
+                write_u32(&mut writer, b.len() as u32)?;
+                for bit in b {
+                    write_u32(&mut writer, bit.witness_index())?;
+                }
+
+                write_u32(&mut writer, *byte_size)?;
+            }
+        };
+
+        Ok(())
+    }
+
+    pub fn read<R: Read>(mut reader: R) -> std::io::Result<Self> {
+        let directive_index = read_u16(&mut reader)?;
+
+        match directive_index {
+            0 => {
+                let x = Witness(read_u32(&mut reader)?);
+                let result = Witness(read_u32(&mut reader)?);
+                Ok(Directive::Invert { x, result })
+            }
+            1 => {
+                let a = Expression::read(&mut reader)?;
+                let b = Expression::read(&mut reader)?;
+                let q = Witness(read_u32(&mut reader)?);
+                let r = Witness(read_u32(&mut reader)?);
+
+                // Read byte to figure out if there is a predicate
+                let predicate_is_some = read_n::<1, _>(&mut reader)?[0] != 0;
+                let predicate = match predicate_is_some {
+                    true => Some(Box::new(Expression::read(&mut reader)?)),
+                    false => None,
+                };
+
+                return Ok(Directive::Quotient {
+                    a,
+                    b,
+                    q,
+                    r,
+                    predicate,
+                });
+            }
+            2 => {
+                let a = Witness(read_u32(&mut reader)?);
+                let b = Witness(read_u32(&mut reader)?);
+                let c = Witness(read_u32(&mut reader)?);
+                let bit_size = read_u32(&mut reader)?;
+                Ok(Directive::Truncate { a, b, c, bit_size })
+            }
+            3 => {
+                let a = Witness(read_u32(&mut reader)?);
+                let b = Witness(read_u32(&mut reader)?);
+                let r = Witness(read_u32(&mut reader)?);
+                let bit_size = read_u32(&mut reader)?;
+                Ok(Directive::Oddrange { a, b, r, bit_size })
+            }
+            4 => {
+                let a = Expression::read(&mut reader)?;
+                let b_len = read_u32(&mut reader)?;
+                let mut b = Vec::with_capacity(b_len as usize);
+                for _ in 0..b_len {
+                    let witness = Witness(read_u32(&mut reader)?);
+                    b.push(witness)
+                }
+
+                let bit_size = read_u32(&mut reader)?;
+
+                Ok(Directive::ToBits { a, b, bit_size })
+            }
+            5 => {
+                let a = Expression::read(&mut reader)?;
+                let b_len = read_u32(&mut reader)?;
+                let mut b = Vec::with_capacity(b_len as usize);
+                for _ in 0..b_len {
+                    let witness = Witness(read_u32(&mut reader)?);
+                    b.push(witness)
+                }
+
+                let byte_size = read_u32(&mut reader)?;
+
+                Ok(Directive::ToBytes { a, b, byte_size })
+            }
+            _ => Err(std::io::ErrorKind::InvalidData.into()),
         }
     }
 }
