fd400c68be7da9c54e08a80e59bd8eb2d568daf6
TomAFrench <tom@tomfren.ch>
2023-02-22 10:53:55 +0000

    feat(acvm)!: add `eth_contract_from_vk` to `SmartContract
    chore: deprecate `eth_contract_from_cs`

--- acvm/src/lib.rs
+++ acvm/src/lib.rs
@@ -115,13 +115,6 @@ pub trait PartialWitnessGenerator {
 }
 
 pub trait SmartContract {
-    // Takes a verification  key and produces a smart contract
-    // The platform indicator allows a backend to support multiple smart contract platforms
-    //
-    // fn verification_key(&self, platform: u8, vk: &[u8]) -> &[u8] {
-    //     todo!("currently the backend is not configured to use this.")
-    // }
-
     /// Takes an ACIR circuit, the number of witnesses and the number of public inputs
     /// Then returns an Ethereum smart contract
     ///
@@ -129,7 +122,11 @@ pub trait SmartContract {
     /// This deprecation may happen in two stages:
     /// The first stage will remove `num_witnesses` and `num_public_inputs` parameters.
     /// If we cannot avoid `num_witnesses`, it can be added into the Circuit struct.
+    #[deprecated]
     fn eth_contract_from_cs(&self, circuit: Circuit) -> String;
+
+    /// Returns an Ethereum smart contract to verify proofs against a given verification key.
+    fn eth_contract_from_vk(&self, verification_key: &[u8]) -> String;
 }
 
 pub trait ProofSystemCompiler {
