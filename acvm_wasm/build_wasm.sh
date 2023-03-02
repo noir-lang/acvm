f27764d10c9eed7da5805c18751c2753cc083267
Joss <joss@aztecprotocol.com>
2023-03-02 20:12:44 +0000

    - switch build to release (300KB vs 18MB) - fix package types path

--- acvm_wasm/build_wasm.sh
+++ acvm_wasm/build_wasm.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-cargo wasi build --target wasm32-unknown-unknown
-wasm-tools component new ../target/wasm32-unknown-unknown/debug/acvm_wasm.wasm \
-    -o ../target/wasm32-unknown-unknown/debug/acvm.component.wasm --adapt ./wasi_snapshot_preview1.wasm
+cargo wasi build --target wasm32-unknown-unknown --release
+wasm-tools component new ../target/wasm32-unknown-unknown/release/acvm_wasm.wasm \
+    -o ../target/wasm32-unknown-unknown/release/acvm.component.wasm --adapt ./wasi_snapshot_preview1.wasm
