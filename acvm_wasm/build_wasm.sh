ee9051398917524dc0e01d1470b1b172eda46c16
Joss <joss@aztecprotocol.com>
2023-03-02 11:28:35 +0000

    mv wasm and wasm wrapper output dirs

--- acvm_wasm/build_wasm.sh
+++ acvm_wasm/build_wasm.sh
@@ -2,4 +2,4 @@
 
 cargo wasi build --target wasm32-unknown-unknown
 wasm-tools component new ../target/wasm32-unknown-unknown/debug/acvm_wasm.wasm \
-    -o acvm.wasm --adapt ./wasi_snapshot_preview1.wasm
+    -o ../target/wasm32-unknown-unknown/debug/acvm.component.wasm --adapt ./wasi_snapshot_preview1.wasm
