b8b0e03c88d1efe59f22fe1a5b1e8ed00ee1c05d
guipublic <47281315+guipublic@users.noreply.github.com>
2023-03-03 17:41:10 +0100

    chore!(acir): remove is_little_endian from to_radix (#129)
    * remove is_little_indian from to_radix
    
    * code review
    
    * code review

--- acvm/src/pwg/directives.rs
+++ acvm/src/pwg/directives.rs
@@ -8,7 +8,7 @@ use acir::{
 use num_bigint::BigUint;
 use num_traits::{One, Zero};
 
-use crate::{OpcodeNotSolvable, OpcodeResolutionError};
+use crate::OpcodeResolutionError;
 
 use super::{get_value, insert_value, sorting::route, witness_to_value};
 
@@ -76,49 +76,30 @@ pub fn solve_directives(
 
             Ok(())
         }
-        Directive::ToRadix { a, b, radix, is_little_endian } => {
+        Directive::ToLeRadix { a, b, radix } => {
             let value_a = get_value(a, initial_witness)?;
             let big_integer = BigUint::from_bytes_be(&value_a.to_be_bytes());
 
-            if *is_little_endian {
-                // Decompose the integer into its radix digits in little endian form.
-                let decomposed_integer = big_integer.to_radix_le(*radix);
+            // Decompose the integer into its radix digits in little endian form.
+            let decomposed_integer = big_integer.to_radix_le(*radix);
 
-                if b.len() < decomposed_integer.len() {
-                    return Err(OpcodeResolutionError::UnsatisfiedConstrain);
-                }
-
-                for (i, witness) in b.iter().enumerate() {
-                    // Fetch the `i'th` digit from the decomposed integer list
-                    // and convert it to a field element.
-                    // If it is not available, which can happen when the decomposed integer
-                    // list is shorter than the witness list, we return 0.
-                    let value = match decomposed_integer.get(i) {
-                        Some(digit) => FieldElement::from_be_bytes_reduce(&[*digit]),
-                        None => FieldElement::zero(),
-                    };
-
-                    insert_value(witness, value, initial_witness)?
-                }
-            } else {
-                // Decompose the integer into its radix digits in big endian form.
-                let decomposed_integer = big_integer.to_radix_be(*radix);
-
-                // if it is big endian and the decompoased integer list is shorter
-                // than the witness list, pad the extra part with 0 first then
-                // add the decompsed interger list to the witness list.
-                let padding_len = b.len() - decomposed_integer.len();
-                let mut value = FieldElement::zero();
-                for (i, witness) in b.iter().enumerate() {
-                    if i >= padding_len {
-                        value = match decomposed_integer.get(i - padding_len) {
-                            Some(digit) => FieldElement::from_be_bytes_reduce(&[*digit]),
-                            None => return Err(OpcodeNotSolvable::UnreachableCode.into()),
-                        };
-                    }
-                    insert_value(witness, value, initial_witness)?
-                }
+            if b.len() < decomposed_integer.len() {
+                return Err(OpcodeResolutionError::UnsatisfiedConstrain);
             }
+
+            for (i, witness) in b.iter().enumerate() {
+                // Fetch the `i'th` digit from the decomposed integer list
+                // and convert it to a field element.
+                // If it is not available, which can happen when the decomposed integer
+                // list is shorter than the witness list, we return 0.
+                let value = match decomposed_integer.get(i) {
+                    Some(digit) => FieldElement::from_be_bytes_reduce(&[*digit]),
+                    None => FieldElement::zero(),
+                };
+
+                insert_value(witness, value, initial_witness)?
+            }
+
             Ok(())
         }
         Directive::OddRange { a, b, r, bit_size } => {
