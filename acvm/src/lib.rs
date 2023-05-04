982678cb392416644b56286081f283f9639598da
Blaine Bublitz <blaine.bublitz@gmail.com>
2023-05-04 13:00:43 -0700

    fmt

--- acvm/src/lib.rs
+++ acvm/src/lib.rs
@@ -52,7 +52,7 @@ pub enum OpcodeResolutionError {
     UnexpectedOpcode(&'static str, BlackBoxFunc),
     #[error("expected {0} inputs for function {1}, but got {2}")]
     IncorrectNumFunctionArguments(usize, BlackBoxFunc, usize),
-    #[error("failed to solve blackbox function: {0}, reason: {1}")] 
+    #[error("failed to solve blackbox function: {0}, reason: {1}")]
     BlackBoxFunctionFailed(BlackBoxFunc, String),
 }
 
