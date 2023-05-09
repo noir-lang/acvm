66819b98b6f34bfa087d5a0769be5754443660b7
Tom French <tom@tomfren.ch>
2023-05-09 18:36:26 +0100

    feat: add `BackendError` to replace trait errors

--- acvm/src/lib.rs
+++ acvm/src/lib.rs
@@ -58,6 +58,22 @@ pub trait Backend:
 {
 }
 
+#[derive(Debug, Error)]
+pub enum BackendError {
+    #[error("could not deserialize proving key")]
+    InvalidProvingKey(Vec<u8>),
+    #[error("could not deserialize verification key")]
+    InvalidVerificationKey(Vec<u8>),
+    #[error("encountered malformed opcode")]
+    MalformedOpcode(Opcode),
+    #[error("called with unexpected opcode")]
+    UnexpectedOpcode(Opcode),
+
+    /// A catch-all error variant for errors which are specific to the backend in question.
+    #[error(transparent)]
+    BackendSpecific(Box<dyn std::error::Error + Send + Sync>),
+}
+
 /// This component will generate the backend specific output for
 /// each OPCODE.
 /// Returns an Error if the backend does not support that OPCODE
@@ -142,19 +158,13 @@ pub trait PartialWitnessGenerator {
 }
 
 pub trait SmartContract {
-    /// The Error type returned by failed function calls in the SmartContract trait.
-    type Error: std::error::Error; // fully-qualified named because thiserror is `use`d at the top of the crate
-
     // TODO: Allow a backend to support multiple smart contract platforms
 
     /// Returns an Ethereum smart contract to verify proofs against a given verification key.
-    fn eth_contract_from_vk(&self, verification_key: &[u8]) -> Result<String, Self::Error>;
+    fn eth_contract_from_vk(&self, verification_key: &[u8]) -> Result<String, BackendError>;
 }
 
 pub trait ProofSystemCompiler {
-    /// The Error type returned by failed function calls in the ProofSystemCompiler trait.
-    type Error: std::error::Error; // fully-qualified named because thiserror is `use`d at the top of the crate
-
     /// The NPC language that this proof system directly accepts.
     /// It is possible for ACVM to transpile to different languages, however it is advised to create a new backend
     /// as this in most cases will be inefficient. For this reason, we want to throw a hard error
@@ -165,11 +175,11 @@ pub trait ProofSystemCompiler {
     fn black_box_function_supported(&self, opcode: &BlackBoxFunc) -> bool;
 
     /// Returns the number of gates in a circuit
-    fn get_exact_circuit_size(&self, circuit: &Circuit) -> Result<u32, Self::Error>;
+    fn get_exact_circuit_size(&self, circuit: &Circuit) -> Result<u32, BackendError>;
 
     /// Generates a proving and verification key given the circuit description
     /// These keys can then be used to construct a proof and for its verification
-    fn preprocess(&self, circuit: &Circuit) -> Result<(Vec<u8>, Vec<u8>), Self::Error>;
+    fn preprocess(&self, circuit: &Circuit) -> Result<(Vec<u8>, Vec<u8>), BackendError>;
 
     /// Creates a Proof given the circuit description, the initial witness values, and the proving key
     /// It is important to note that the intermediate witnesses for black box functions will not generated
@@ -179,7 +189,7 @@ pub trait ProofSystemCompiler {
         circuit: &Circuit,
         witness_values: BTreeMap<Witness, FieldElement>,
         proving_key: &[u8],
-    ) -> Result<Vec<u8>, Self::Error>;
+    ) -> Result<Vec<u8>, BackendError>;
 
     /// Verifies a Proof, given the circuit description, the circuit's public inputs, and the verification key
     fn verify_with_vk(
@@ -188,7 +198,7 @@ pub trait ProofSystemCompiler {
         public_inputs: BTreeMap<Witness, FieldElement>,
         circuit: &Circuit,
         verification_key: &[u8],
-    ) -> Result<bool, Self::Error>;
+    ) -> Result<bool, BackendError>;
 }
 
 /// Supported NP complete languages
