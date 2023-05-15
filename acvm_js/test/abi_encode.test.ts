5058791a9510286302fab42b0894e4a90f215970
Tom French <tom@tomfren.ch>
2023-05-15 11:15:30 +0100

    chore: apply eslint fixes

--- acvm_js/test/abi_encode.test.ts
+++ acvm_js/test/abi_encode.test.ts
@@ -1,26 +1,32 @@
-import { expect, test } from "@jest/globals"
-import { abi_encode, abi_decode } from "../pkg/"
+import { expect, test } from "@jest/globals";
+import { abi_encode, abi_decode } from "../pkg/";
 
-test('recovers original inputs when abi encoding and decoding', () => {
+test("recovers original inputs when abi encoding and decoding", () => {
   // TODO use ts-rs to get ABI type bindings.
   const abi = {
     parameters: [
-      {name: "foo", type: { kind: "field" }, visibility: "private"},
-      {name: "bar", type: { kind: "array", length: 2, type: { kind: "field" } }, visibility: "private"}
+      { name: "foo", type: { kind: "field" }, visibility: "private" },
+      {
+        name: "bar",
+        type: { kind: "array", length: 2, type: { kind: "field" } },
+        visibility: "private",
+      },
     ],
-    param_witnesses: {"foo": [1], "bar": [2, 3]},
+    param_witnesses: { foo: [1], bar: [2, 3] },
     return_type: null,
-    return_witnesses: []
+    return_witnesses: [],
   };
-  const inputs = { 
+  const inputs = {
     foo: "1",
-    bar: ["1", "2"]
+    bar: ["1", "2"],
   };
   const initial_witness: Map<string, string> = abi_encode(abi, inputs, null);
-  const decoded_inputs: {inputs: Record<string, any>, return_value: any} = abi_decode(abi, initial_witness);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const decoded_inputs: { inputs: Record<string, any>; return_value: any } =
+    abi_decode(abi, initial_witness);
 
-  expect(BigInt(decoded_inputs.inputs.foo)).toBe(BigInt(inputs.foo))
-  expect(BigInt(decoded_inputs.inputs.bar[0])).toBe(BigInt(inputs.bar[0]))
-  expect(BigInt(decoded_inputs.inputs.bar[1])).toBe(BigInt(inputs.bar[1]))
-  expect(decoded_inputs.return_value).toBe(null)
+  expect(BigInt(decoded_inputs.inputs.foo)).toBe(BigInt(inputs.foo));
+  expect(BigInt(decoded_inputs.inputs.bar[0])).toBe(BigInt(inputs.bar[0]));
+  expect(BigInt(decoded_inputs.inputs.bar[1])).toBe(BigInt(inputs.bar[1]));
+  expect(decoded_inputs.return_value).toBe(null);
 });
--- acvm_js/test/execute_circuit.test.ts
+++ acvm_js/test/execute_circuit.test.ts
@@ -1,52 +1,84 @@
-import { expect, test } from "@jest/globals"
-import { abi_encode, abi_decode, execute_circuit } from "../pkg/"
+import { expect, test } from "@jest/globals";
+import { abi_encode, abi_decode, execute_circuit } from "../pkg/";
 
-
-test('successfully executes circuit and extracts return value', async () => {
+test("successfully executes circuit and extracts return value", async () => {
   // Noir program which enforces that x != y and returns x + y.
   const abi = {
-    parameters:[
-      { name:"x", type: { kind: "field" }, visibility:"private" },
-      { name:"y", type: { kind: "field" }, visibility:"public" }
+    parameters: [
+      { name: "x", type: { kind: "field" }, visibility: "private" },
+      { name: "y", type: { kind: "field" }, visibility: "public" },
     ],
-    param_witnesses:
-      {
-        x: [1],
-        y: [2]
-      },
+    param_witnesses: {
+      x: [1],
+      y: [2],
+    },
     return_type: { kind: "field" },
-    return_witnesses: [6]
+    return_witnesses: [6],
   };
-  const bytecode = Uint8Array.from([0,0,0,0,7,0,0,0,1,0,0,0,2,0,0,0,1,0,0,0,6,0,0,0,6,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,48,100,78,114,225,49,160,41,184,80,69,182,129,129,88,93,40,51,232,72,121,185,112,145,67,225,245,147,240,0,0,0,2,0,0,0,48,100,78,114,225,49,160,41,184,80,69,182,129,129,88,93,40,51,232,72,121,185,112,145,67,225,245,147,240,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,3,0,0,0,4,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,3,0,0,0,4,0,0,0,48,100,78,114,225,49,160,41,184,80,69,182,129,129,88,93,40,51,232,72,121,185,112,145,67,225,245,147,240,0,0,0,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,3,0,0,0,5,0,0,0,48,100,78,114,225,49,160,41,184,80,69,182,129,129,88,93,40,51,232,72,121,185,112,145,67,225,245,147,240,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,48,100,78,114,225,49,160,41,184,80,69,182,129,129,88,93,40,51,232,72,121,185,112,145,67,225,245,147,240,0,0,0,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,2,0,0,0,48,100,78,114,225,49,160,41,184,80,69,182,129,129,88,93,40,51,232,72,121,185,112,145,67,225,245,147,240,0,0,0,6,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
+  const bytecode = Uint8Array.from([
+    0, 0, 0, 0, 7, 0, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 1, 0, 0, 0, 6, 0, 0, 0, 6,
+    0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 48,
+    100, 78, 114, 225, 49, 160, 41, 184, 80, 69, 182, 129, 129, 88, 93, 40, 51,
+    232, 72, 121, 185, 112, 145, 67, 225, 245, 147, 240, 0, 0, 0, 2, 0, 0, 0,
+    48, 100, 78, 114, 225, 49, 160, 41, 184, 80, 69, 182, 129, 129, 88, 93, 40,
+    51, 232, 72, 121, 185, 112, 145, 67, 225, 245, 147, 240, 0, 0, 0, 3, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 3, 0, 0, 0, 4, 0, 0, 0, 0, 1, 0, 0, 0, 1,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 3, 0, 0, 0, 4, 0, 0, 0, 48, 100, 78, 114, 225,
+    49, 160, 41, 184, 80, 69, 182, 129, 129, 88, 93, 40, 51, 232, 72, 121, 185,
+    112, 145, 67, 225, 245, 147, 240, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 3, 0, 0, 0, 5, 0, 0, 0, 48,
+    100, 78, 114, 225, 49, 160, 41, 184, 80, 69, 182, 129, 129, 88, 93, 40, 51,
+    232, 72, 121, 185, 112, 145, 67, 225, 245, 147, 240, 0, 0, 0, 3, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 48, 100, 78, 114, 225, 49, 160,
+    41, 184, 80, 69, 182, 129, 129, 88, 93, 40, 51, 232, 72, 121, 185, 112, 145,
+    67, 225, 245, 147, 240, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0,
+    0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2,
+    0, 0, 0, 48, 100, 78, 114, 225, 49, 160, 41, 184, 80, 69, 182, 129, 129, 88,
+    93, 40, 51, 232, 72, 121, 185, 112, 145, 67, 225, 245, 147, 240, 0, 0, 0, 6,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  ]);
 
-  
-  const inputs = { 
+  const inputs = {
     x: "1",
-    y: "2"
+    y: "2",
   };
-  const return_witness: number = abi.return_witnesses[0]
+  const return_witness: number = abi.return_witnesses[0];
 
   const initial_witness: Map<number, string> = abi_encode(abi, inputs, null);
-  const solved_witness: Map<number, string> = await execute_circuit(bytecode, initial_witness, () => {throw Error("unexpected oracle")})
-  
+  const solved_witness: Map<number, string> = await execute_circuit(
+    bytecode,
+    initial_witness,
+    () => {
+      throw Error("unexpected oracle");
+    }
+  );
+
   // Solved witness should be consistent with initial witness
   initial_witness.forEach((value, key) => {
-    expect(solved_witness.get(key) as string).toBe(value)
-
-  }) 
+    expect(solved_witness.get(key) as string).toBe(value);
+  });
   // Solved witness should contain expected return value
-  expect(BigInt(solved_witness.get(return_witness) as string)).toBe(3n)
+  expect(BigInt(solved_witness.get(return_witness) as string)).toBe(3n);
 
   const decoded_inputs = abi_decode(abi, solved_witness);
 
-  expect(BigInt(decoded_inputs.return_value)).toBe(3n)
+  expect(BigInt(decoded_inputs.return_value)).toBe(3n);
 });
 
-
-test('successfully processes oracle opcodes', async () => {
+test("successfully processes oracle opcodes", async () => {
   // We use a handwritten circuit which uses an oracle to calculate the sum of witnesses 1 and 2
   // and stores the result in witness 3. This is then enforced by an arithmetic opcode to check the result is correct.
-  
+
   // let oracle = OracleData {
   //     name: "example_oracle".to_owned(),
   //     inputs: vec![Witness(1).into(), Witness(2).into()],
@@ -70,24 +102,52 @@ test('successfully processes oracle opcodes', async () => {
   //     public_parameters: PublicInputs::default(),
   //     return_values: PublicInputs::default(),
   // };
-  let oracle_bytecode = new Uint8Array([0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 6, 14, 0, 0, 0, 101, 120, 97, 109, 112, 108, 101, 95, 111, 114, 97, 99, 108, 101, 2, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0, 0, 48, 100, 78, 114, 225, 49, 160, 41, 184, 80, 69, 182, 129, 129, 88, 93, 40, 51, 232, 72, 121, 185, 112, 145, 67, 225, 245, 147, 240, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  const oracle_bytecode = new Uint8Array([
+    0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 6, 14, 0, 0, 0,
+    101, 120, 97, 109, 112, 108, 101, 95, 111, 114, 97, 99, 108, 101, 2, 0, 0,
+    0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 1, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0, 0,
+    48, 100, 78, 114, 225, 49, 160, 41, 184, 80, 69, 182, 129, 129, 88, 93, 40,
+    51, 232, 72, 121, 185, 112, 145, 67, 225, 245, 147, 240, 0, 0, 0, 3, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+  ]);
 
-  const initial_witness: Map<number, string> = new Map()
-  initial_witness.set(1, "0x0000000000000000000000000000000000000000000000000000000000000001");
-  initial_witness.set(2, "0x0000000000000000000000000000000000000000000000000000000000000001");
-  
-  const solved_witness: Map<number, string> = await execute_circuit(oracle_bytecode, initial_witness, async (name: string, inputs: string[]) => { 
-    // We cannot use jest matchers here (or write to a variable in the outside scope) so cannot test that 
-    // the values for `name` and `inputs` are correct, we can `console.log` them however.
-    // console.log(name)
-    // console.log(inputs)
+  const initial_witness: Map<number, string> = new Map();
+  initial_witness.set(
+    1,
+    "0x0000000000000000000000000000000000000000000000000000000000000001"
+  );
+  initial_witness.set(
+    2,
+    "0x0000000000000000000000000000000000000000000000000000000000000001"
+  );
 
-    // Witness(1) + Witness(2) = 1 + 1 = 2
-    return ["0x02"]
-  })
+  const solved_witness: Map<number, string> = await execute_circuit(
+    oracle_bytecode,
+    initial_witness,
+    async (_name: string, _inputs: string[]) => {
+      // We cannot use jest matchers here (or write to a variable in the outside scope) so cannot test that
+      // the values for `name` and `inputs` are correct, we can `console.log` them however.
+      // console.log(name)
+      // console.log(inputs)
+
+      // Witness(1) + Witness(2) = 1 + 1 = 2
+      return ["0x02"];
+    }
+  );
 
   // If incorrect value is written into circuit then execution should halt due to unsatisfied constraint in
   // arithmetic opcode. Nevertheless, check that returned value was inserted correctly.
-  expect(solved_witness.get(3) as string).toBe("0x0000000000000000000000000000000000000000000000000000000000000002")
+  expect(solved_witness.get(3) as string).toBe(
+    "0x0000000000000000000000000000000000000000000000000000000000000002"
+  );
 });
-
