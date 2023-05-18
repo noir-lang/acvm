58d4fd85138bdc4a67eeafa702b4f09dcabb28f0
ludamad <domuradical@gmail.com>
2023-05-18 17:19:25 -0400

    cargo fmt

--- acvm/src/pwg/brillig.rs
+++ acvm/src/pwg/brillig.rs
@@ -109,7 +109,7 @@ impl BrilligSolver {
             input_registers,
             input_memory,
             brillig.bytecode.clone(),
-            brillig.foreign_call_results.clone()
+            brillig.foreign_call_results.clone(),
         );
 
         let vm_status = vm.process_opcodes();
