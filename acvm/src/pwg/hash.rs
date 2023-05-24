0f7244bfad3fac75e46c4c92ec1ce61436bc00ee
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2023-05-24 17:23:45 +0100

    - Modify generic hash to take a new parameter that allows us to truncate the message input. - Add function to solve KeccakVar

--- acvm/src/pwg/hash.rs
+++ acvm/src/pwg/hash.rs
@@ -16,7 +16,7 @@ pub fn blake2s256(
     inputs: &[FunctionInput],
     outputs: &[Witness],
 ) -> Result<OpcodeResolution, OpcodeResolutionError> {
-    let hash = generic_hash_256::<Blake2s256>(initial_witness, inputs)?;
+    let hash = generic_hash_256::<Blake2s256>(initial_witness, None, inputs)?;
 
     for (output_witness, value) in outputs.iter().zip(hash.iter()) {
         insert_value(
@@ -34,7 +34,7 @@ pub fn sha256(
     inputs: &[FunctionInput],
     outputs: &[Witness],
 ) -> Result<OpcodeResolution, OpcodeResolutionError> {
-    let hash = generic_hash_256::<Sha256>(initial_witness, inputs)?;
+    let hash = generic_hash_256::<Sha256>(initial_witness, None, inputs)?;
 
     for (output_witness, value) in outputs.iter().zip(hash.iter()) {
         insert_value(
@@ -52,7 +52,26 @@ pub fn keccak256(
     inputs: &[FunctionInput],
     outputs: &[Witness],
 ) -> Result<OpcodeResolution, OpcodeResolutionError> {
-    let hash = generic_hash_256::<Keccak256>(initial_witness, inputs)?;
+    let hash = generic_hash_256::<Keccak256>(initial_witness, None, inputs)?;
+
+    for (output_witness, value) in outputs.iter().zip(hash.iter()) {
+        insert_value(
+            output_witness,
+            FieldElement::from_be_bytes_reduce(&[*value]),
+            initial_witness,
+        )?;
+    }
+
+    Ok(OpcodeResolution::Solved)
+}
+
+pub fn keccak256_variable_length(
+    initial_witness: &mut WitnessMap,
+    inputs: &[FunctionInput],
+    var_message_size: FunctionInput,
+    outputs: &[Witness],
+) -> Result<OpcodeResolution, OpcodeResolutionError> {
+    let hash = generic_hash_256::<Keccak256>(initial_witness, Some(var_message_size), inputs)?;
 
     for (output_witness, value) in outputs.iter().zip(hash.iter()) {
         insert_value(
@@ -70,7 +89,7 @@ pub fn hash_to_field_128_security(
     inputs: &[FunctionInput],
     output: &Witness,
 ) -> Result<OpcodeResolution, OpcodeResolutionError> {
-    let hash = generic_hash_256::<Blake2s256>(initial_witness, inputs)?;
+    let hash = generic_hash_256::<Blake2s256>(initial_witness, None, inputs)?;
 
     let reduced_res = FieldElement::from_be_bytes_reduce(&hash);
     insert_value(output, reduced_res, initial_witness)?;
@@ -80,9 +99,12 @@ pub fn hash_to_field_128_security(
 
 fn generic_hash_256<D: Digest>(
     initial_witness: &mut WitnessMap,
+    num_bytes_to_truncate_message: Option<FunctionInput>,
     inputs: &[FunctionInput],
 ) -> Result<[u8; 32], OpcodeResolutionError> {
     let mut hasher = D::new();
+
+    let mut message_input = Vec::new();
 
     // Read witness assignments into hasher.
     for input in inputs.iter() {
@@ -91,7 +113,19 @@ fn generic_hash_256<D: Digest>(
 
         let witness_assignment = witness_to_value(initial_witness, witness)?;
         let bytes = witness_assignment.fetch_nearest_bytes(num_bits);
-        hasher.update(bytes);
+        message_input.extend(bytes);
+    }
+
+    // Truncate the message if there is a message_size parameter given
+    match num_bytes_to_truncate_message {
+        Some(input) => {
+            let num_bytes_to_take =
+                witness_to_value(initial_witness, input.witness)?.to_u128() as usize;
+
+            let truncated_message = &message_input[0..num_bytes_to_take];
+            hasher.update(truncated_message)
+        }
+        None => hasher.update(message_input),
     }
 
     let result = hasher.finalize().as_slice().try_into().unwrap();
