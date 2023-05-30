64a6db4b19d936c0868e1487078543a4919bfa4f
Maxim Vezenov <mvezenov@gmail.com>
2023-05-30 16:07:09 -0400

    remove unnecessary method for fetching WitnessMap values

--- acir/src/native_types/witness_map.rs
+++ acir/src/native_types/witness_map.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::{btree_map, btree_map::Values, BTreeMap},
+    collections::{btree_map, BTreeMap},
     io::Read,
     ops::Index,
 };
@@ -49,9 +49,6 @@ impl WitnessMap {
     }
     pub fn insert(&mut self, key: Witness, value: FieldElement) -> Option<FieldElement> {
         self.0.insert(key, value)
-    }
-    pub fn values(&self) -> Values<'_, Witness, FieldElement> {
-        self.0.values()
     }
 }
 
