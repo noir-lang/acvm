d2d82a4ae4c40b357ef1f45ccf4b7b7cfacae8f5
TomAFrench <tom@tomfren.ch>
2023-06-05 11:11:53 +0000

    feat(acir): implement arithmetic operators on `Witness`

--- acir/src/native_types/witness.rs
+++ acir/src/native_types/witness.rs
@@ -1,4 +1,9 @@
+use std::ops::{Add, Mul, Sub};
+
+use acir_field::FieldElement;
 use serde::{Deserialize, Serialize};
+
+use super::Expression;
 
 // Witness might be a misnomer. This is an index that represents the position a witness will take
 #[derive(
@@ -26,5 +31,66 @@ impl Witness {
 impl From<u32> for Witness {
     fn from(value: u32) -> Self {
         Self(value)
+    }
+}
+
+// Operators
+
+impl Add for Witness {
+    type Output = Expression;
+    fn add(self, rhs: Witness) -> Self::Output {
+        if self == rhs {
+            Expression {
+                mul_terms: Vec::new(),
+                linear_combinations: vec![(FieldElement::from(2_u128), self)],
+                q_c: FieldElement::zero(),
+            }
+        } else {
+            let linear_combinations = if self < rhs {
+                vec![(FieldElement::one(), self), (FieldElement::one(), rhs)]
+            } else {
+                vec![(FieldElement::one(), rhs), (FieldElement::one(), self)]
+            };
+
+            Expression { mul_terms: Vec::new(), linear_combinations, q_c: FieldElement::zero() }
+        }
+    }
+}
+
+impl Sub for Witness {
+    type Output = Expression;
+    fn sub(self, rhs: Witness) -> Self::Output {
+        if self == rhs {
+            Expression::zero()
+        } else {
+            let linear_combinations = if self < rhs {
+                vec![(FieldElement::one(), self), (-FieldElement::one(), rhs)]
+            } else {
+                vec![(-FieldElement::one(), rhs), (FieldElement::one(), self)]
+            };
+
+            Expression { mul_terms: Vec::new(), q_c: FieldElement::zero(), linear_combinations }
+        }
+    }
+}
+
+impl Mul for Witness {
+    type Output = Expression;
+    fn mul(self, rhs: Witness) -> Self::Output {
+        if self == rhs {
+            Expression {
+                mul_terms: vec![(FieldElement::one(), self, self)],
+                linear_combinations: vec![],
+                q_c: FieldElement::zero(),
+            }
+        } else {
+            let mul_terms = if self < rhs {
+                vec![(FieldElement::one(), self, rhs)]
+            } else {
+                vec![(FieldElement::one(), rhs, self)]
+            };
+
+            Expression { mul_terms, linear_combinations: Vec::new(), q_c: FieldElement::zero() }
+        }
     }
 }
