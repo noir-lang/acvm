d50a6fc027829dc5d3c358d60bc01347abf22f73
Tom French <15848336+TomAFrench@users.noreply.github.com>
2023-06-19 18:15:42 +0700

    chore: add automatic symlinks for build artifacts (https://github.com/noir-lang/acvm-simulator/pull/52)

--- acvm_js/build.sh
+++ acvm_js/build.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
 
+rm -rf ./outputs >/dev/null 2>&1
+rm -rf ./result >/dev/null 2>&1
+
+if [ -v out ]; then
+  echo "Will install package to $out (defined outside installPhase.sh script)"
+else
+  out="./outputs/out"
+  echo "Will install package to $out"
+fi
+
 ./buildPhaseCargoCommand.sh
 ./installPhase.sh
+
+ln -s $out ./result
--- acvm_js/buildPhaseCargoCommand.sh
+++ acvm_js/buildPhaseCargoCommand.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 # Clear out the existing build artifacts as these aren't automatically removed by wasm-pack.
 if [ -d ./pkg/ ]; then
     rm -rf ./pkg/
