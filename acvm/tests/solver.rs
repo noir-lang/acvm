b139d4d566c715009465a430aab0fb819aacab4f
Tom French <15848336+TomAFrench@users.noreply.github.com>
2023-06-21 10:48:12 +0700

    feat(acir)!: Use fixed length data structures in black box function inputs/outputs where possible. (#386)

--- acvm/tests/solver.rs
+++ acvm/tests/solver.rs
@@ -26,11 +26,12 @@ impl PartialWitnessGenerator for StubbedPwg {
     fn schnorr_verify(
         &self,
         _initial_witness: &mut WitnessMap,
-        _public_key_x: &FunctionInput,
-        _public_key_y: &FunctionInput,
-        _signature: &[FunctionInput],
+        _public_key_x: FunctionInput,
+        _public_key_y: FunctionInput,
+        _signature_s: FunctionInput,
+        _signature_e: FunctionInput,
         _message: &[FunctionInput],
-        _output: &Witness,
+        _output: Witness,
     ) -> Result<OpcodeResolution, OpcodeResolutionError> {
         panic!("Path not trodden by this test")
     }
@@ -40,7 +41,7 @@ impl PartialWitnessGenerator for StubbedPwg {
         _initial_witness: &mut WitnessMap,
         _inputs: &[FunctionInput],
         _domain_separator: u32,
-        _outputs: &[Witness],
+        _outputs: (Witness, Witness),
     ) -> Result<OpcodeResolution, OpcodeResolutionError> {
         panic!("Path not trodden by this test")
     }
@@ -48,8 +49,8 @@ impl PartialWitnessGenerator for StubbedPwg {
     fn fixed_base_scalar_mul(
         &self,
         _initial_witness: &mut WitnessMap,
-        _input: &FunctionInput,
-        _outputs: &[Witness],
+        _input: FunctionInput,
+        _outputs: (Witness, Witness),
     ) -> Result<OpcodeResolution, OpcodeResolutionError> {
         panic!("Path not trodden by this test")
     }
