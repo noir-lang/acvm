fbba92b13ad3827304789ab16f358c55404931ae
ethan-000 <ethan_000@outlook.com>
2023-06-28 16:48:13 +0100

    padding

--- stdlib/src/custom_gate_fallbacks/sha256.rs
+++ stdlib/src/custom_gate_fallbacks/sha256.rs
@@ -1,5 +1,7 @@
 use acir::{
+    brillig_vm::{self, BinaryFieldOp, RegisterIndex},
     circuit::{
+        brillig::{Brillig, BrilligInputs, BrilligOutputs},
         opcodes::{BlackBoxFuncCall, FunctionInput},
         Opcode,
     },
@@ -7,7 +9,7 @@ use acir::{
     FieldElement,
 };
 
-use crate::{fallback::range, helpers::VariableStore};
+use crate::helpers::VariableStore;
 
 use super::utils::{byte_decomposition, round_to_nearest_byte};
 
@@ -32,17 +34,23 @@ pub fn sha256(
         num_witness = updated_witness_counter;
     }
 
-    let output_bytes = create_sha256_constraint(new_inputs, total_num_bytes, num_witness);
-    (0, Vec::new())
+    let (num_witness, extra_gates) =
+        create_sha256_constraint(new_inputs, total_num_bytes, num_witness);
+    new_gates.extend(extra_gates);
+    (num_witness, new_gates)
 }
 
-fn create_sha256_constraint(mut input: Vec<Witness>, total_num_bytes: u32, num_witness: u32) {
+fn create_sha256_constraint(
+    mut input: Vec<Witness>,
+    total_num_bytes: u32,
+    num_witness: u32,
+) -> (u32, Vec<Opcode>) {
     let mut new_gates = Vec::new();
 
     // pad the bytes according to sha256 padding rules.
     let message_bits = total_num_bytes * 8;
-    let (mut num_witness, pad_witness, gates) = pad(128, 8, num_witness);
-    new_gates.extend(gates);
+    let (mut num_witness, pad_witness, extra_gates) = pad(128, 8, num_witness);
+    new_gates.extend(extra_gates);
     input.push(pad_witness);
 
     let bytes_per_block = 64;
@@ -51,15 +59,21 @@ fn create_sha256_constraint(mut input: Vec<Witness>, total_num_bytes: u32, num_w
 
     let num_total_bytes = num_blocks * bytes_per_block;
     for _ in num_bytes..num_total_bytes {
-        let (updated_witness_counter, pad_witness, gates) = pad(0, 8, num_witness);
+        let (updated_witness_counter, pad_witness, extra_gates) = pad(0, 8, num_witness);
         num_witness = updated_witness_counter;
-        new_gates.extend(gates);
+        new_gates.extend(extra_gates);
         input.push(pad_witness);
     }
 
-    let (num_witness, pad_witness, gates) = pad(message_bits, 64, num_witness);
-    new_gates.extend(gates);
-    input.push(pad_witness);
+    let (num_witness, pad_witness, extra_gates) = pad(message_bits, 64, num_witness);
+    new_gates.extend(extra_gates);
+
+    let (extra_gates, pad_witness, num_witness) =
+        byte_decomposition(pad_witness.into(), 8, num_witness);
+    new_gates.extend(extra_gates);
+    input.extend(pad_witness);
+
+    (num_witness, new_gates)
 }
 
 fn pad(number: u32, bit_size: u32, mut num_witness: u32) -> (u32, Witness, Vec<Opcode>) {
@@ -67,10 +81,30 @@ fn pad(number: u32, bit_size: u32, mut num_witness: u32) -> (u32, Witness, Vec<O
     let mut variables = VariableStore::new(&mut num_witness);
 
     let pad = variables.new_variable();
-    let mut pad_expr = Expression::default();
-    pad_expr.push_addition_term(FieldElement::from(number as u128), pad);
-    new_gates.push(Opcode::Arithmetic(pad_expr.clone()));
-    let num_witness = variables.finalize();
+
+    // TODO: Should this padding generated from brillig be constrained
+    let brillig_opcode = Opcode::Brillig(Brillig {
+        inputs: vec![
+            BrilligInputs::Single(Expression {
+                // Input Register 0
+                mul_terms: vec![],
+                linear_combinations: vec![],
+                q_c: FieldElement::from(number as u128),
+            }),
+            BrilligInputs::Single(Expression::default()),
+        ],
+        outputs: vec![BrilligOutputs::Simple(pad)],
+        foreign_call_results: vec![],
+        bytecode: vec![brillig_vm::Opcode::BinaryFieldOp {
+            op: BinaryFieldOp::Add,
+            lhs: RegisterIndex::from(0),
+            rhs: RegisterIndex::from(1),
+            destination: RegisterIndex::from(0),
+        }],
+        predicate: None,
+    });
+    new_gates.push(brillig_opcode);
+
     let range = Opcode::BlackBoxFuncCall(BlackBoxFuncCall::RANGE {
         input: FunctionInput { witness: pad, num_bits: bit_size },
     });
--- stdlib/src/custom_gate_fallbacks/utils.rs
+++ stdlib/src/custom_gate_fallbacks/utils.rs
@@ -1,6 +1,3 @@
-use core::num;
-use std::vec;
-
 use acir::{
     circuit::{
         directives::Directive,
@@ -11,7 +8,7 @@ use acir::{
     FieldElement,
 };
 
-use crate::{fallback::range, helpers::VariableStore};
+use crate::helpers::VariableStore;
 
 fn round_to_nearest_mul_8(num_bits: u32) -> u32 {
     let remainder = num_bits % 8;
@@ -48,6 +45,8 @@ pub(crate) fn byte_decomposition(
         radix: 256,
     }));
 
+    vector.reverse();
+
     // Now apply constraints to the bytes such that they are the byte decomposition
     // of the input and each byte is actually a byte
     let mut byte_exprs = Vec::new();
@@ -60,21 +59,14 @@ pub(crate) fn byte_decomposition(
         let scaling_factor_value = byte_shift.pow(num_bytes - 1 - i as u32);
         let scaling_factor = FieldElement::from(scaling_factor_value as u128);
 
-        decomp_constraint.push_addition_term(scaling_factor, vector[i]);
+        decomp_constraint.push_addition_term(-scaling_factor, vector[i]);
 
         byte_exprs.push(range);
     }
 
     new_gates.extend(byte_exprs);
-    decomp_constraint.sort(); // TODO: we have an issue open to check if this is needed. Ideally, we remove it.
+    decomp_constraint.sort();
     new_gates.push(Opcode::Arithmetic(decomp_constraint));
 
     (new_gates, vector, variables.finalize())
-}
-
-pub(crate) fn split_field_element_to_bytes(element: FieldElement, num_bytes: u32) {
-    assert!(num_bytes <= 32);
-
-    let mut value = element.to_be_bytes();
-    value.resize(num_bytes as usize, 0);
 }
