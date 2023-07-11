d5e433587419a9547f43f61f159266a901063d19
TomAFrench <tom@tomfren.ch>
2023-07-11 02:01:19 +0000

    chore(acvm)!: Remove `CircuitSimplifer`

--- acvm/src/compiler/transformers/fallback.rs
+++ acvm/src/compiler/transformers/fallback.rs
@@ -1,5 +1,3 @@
-use crate::compiler::optimizers::CircuitSimplifier;
-
 use super::super::CompileError;
 use acir::{
     circuit::{opcodes::BlackBoxFuncCall, Circuit, Opcode, OpcodeLabel},
@@ -15,49 +13,42 @@ impl FallbackTransformer {
     pub(crate) fn transform(
         acir: Circuit,
         is_supported: impl Fn(&Opcode) -> bool,
-        simplifier: &CircuitSimplifier,
         opcode_labels: Vec<OpcodeLabel>,
     ) -> Result<(Circuit, Vec<OpcodeLabel>), CompileError> {
         let mut acir_supported_opcodes = Vec::with_capacity(acir.opcodes.len());
         let mut new_opcode_labels = Vec::with_capacity(opcode_labels.len());
         let mut witness_idx = acir.current_witness_index + 1;
-        // add opcodes for defining the witness that will be solved through simplification but must be kept
-        for w in &simplifier.defined {
-            acir_supported_opcodes.push(simplifier.define(w));
-        }
+
         for (idx, opcode) in acir.opcodes.into_iter().enumerate() {
-            if !simplifier.solved_gates.contains(&idx) {
-                match &opcode {
-                    Opcode::Arithmetic(_)
-                    | Opcode::Directive(_)
-                    | Opcode::Brillig(_)
-                    | Opcode::Block(_)
-                    | Opcode::ROM(_)
-                    | Opcode::RAM(_) => {
-                        // directive, arithmetic expression or blocks are handled by acvm
+            match &opcode {
+                Opcode::Arithmetic(_)
+                | Opcode::Directive(_)
+                | Opcode::Brillig(_)
+                | Opcode::Block(_)
+                | Opcode::ROM(_)
+                | Opcode::RAM(_) => {
+                    // directive, arithmetic expression or blocks are handled by acvm
+                    new_opcode_labels.push(opcode_labels[idx]);
+                    acir_supported_opcodes.push(opcode);
+                    continue;
+                }
+                Opcode::BlackBoxFuncCall(bb_func_call) => {
+                    // We know it is an black box function. Now check if it is
+                    // supported by the backend. If it is supported, then we can simply
+                    // collect the opcode
+                    if is_supported(&opcode) {
                         new_opcode_labels.push(opcode_labels[idx]);
                         acir_supported_opcodes.push(opcode);
                         continue;
-                    }
-                    Opcode::BlackBoxFuncCall(bb_func_call) => {
-                        // We know it is an black box function. Now check if it is
-                        // supported by the backend. If it is supported, then we can simply
-                        // collect the opcode
-                        if is_supported(&opcode) {
-                            new_opcode_labels.push(opcode_labels[idx]);
-                            acir_supported_opcodes.push(opcode);
-                            continue;
-                        } else {
-                            // If we get here then we know that this black box function is not supported
-                            // so we need to replace it with a version of the opcode which only uses arithmetic
-                            // expressions
-                            let (updated_witness_index, opcodes_fallback) =
-                                Self::opcode_fallback(bb_func_call, witness_idx)?;
-                            witness_idx = updated_witness_index;
-                            new_opcode_labels
-                                .extend(vec![opcode_labels[idx]; opcodes_fallback.len()]);
-                            acir_supported_opcodes.extend(opcodes_fallback);
-                        }
+                    } else {
+                        // If we get here then we know that this black box function is not supported
+                        // so we need to replace it with a version of the opcode which only uses arithmetic
+                        // expressions
+                        let (updated_witness_index, opcodes_fallback) =
+                            Self::opcode_fallback(bb_func_call, witness_idx)?;
+                        witness_idx = updated_witness_index;
+                        new_opcode_labels.extend(vec![opcode_labels[idx]; opcodes_fallback.len()]);
+                        acir_supported_opcodes.extend(opcodes_fallback);
                     }
                 }
             }
