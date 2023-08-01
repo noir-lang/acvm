973c6bbf4e4210af0cdcf282d17e5ef90b04e2b4
Koby <kobyhall@proton.me>
2023-08-01 19:54:42 +0200

    chore: enable build script to find bb in file system

--- acvm_js/build.rs
+++ acvm_js/build.rs
@@ -1,5 +1,6 @@
 const GIT_COMMIT: &&str = &"GIT_COMMIT";
 const BARRETENBERG_BIN_DIR: &&str = &"BARRETENBERG_BIN_DIR";
+const BB_WASM: &&str = &"barretenberg.wasm";
 
 fn main() -> Result<(), String> {
     if std::env::var(GIT_COMMIT).is_err() {
@@ -18,7 +19,17 @@ fn main() -> Result<(), String> {
                 println!("cargo:rustc-env={BARRETENBERG_BIN_DIR}={bindir}");
                 Ok(())
             } else {
-                Err("Unable to locate barretenberg.wasm - Please set the BARRETENBERG_BIN_DIR env var to the directory where it exists".into())
+                let current_dir = std::env::current_dir().unwrap();
+                let bin_path = current_dir.join("result").join("bin");                
+                let wasm_path = bin_path.join(BB_WASM);
+                let bin_path_string = bin_path.to_string_lossy();
+                if wasm_path.exists() && wasm_path.is_file() {
+                    println!("cargo:warning=BARRETENBERG_BIN_DIR env not set, setting BARRETENBERG_BIN_DIR={bin_path_string}");
+                    println!("cargo:rustc-env={BARRETENBERG_BIN_DIR}={bin_path_string}");
+                    Ok(())
+                } else {
+                    Err(format!("Unable to locate barretenberg.wasm - Please set the BARRETENBERG_BIN_DIR env var to the directory where it exists, or ensure it's located at {bin_path_string}", ).into())
+                }
             }
         }
     }
