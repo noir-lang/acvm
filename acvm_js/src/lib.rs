ad5e449f451e88490d281bc7bfd69425b1f16134
Tom French <tom@tomfren.ch>
2023-08-03 11:15:01 +0100

    chore: remove `#[cfg(target_arch = "wasm32")]` spam in acvm_js

--- acvm_js/src/lib.rs
+++ acvm_js/src/lib.rs
@@ -1,36 +1,27 @@
 // #![warn(unused_crate_dependencies, unused_extern_crates)]
 #![warn(unreachable_pub)]
 
-// TODO: Abscense of Per Pakckage targets
+// TODO: Absence of per package targets
 // https://doc.rust-lang.org/cargo/reference/unstable.html#per-package-target
 // otherwise could be reorganized to make this file more pretty.
 
-#[cfg(target_arch = "wasm32")]
-mod barretenberg;
-#[cfg(target_arch = "wasm32")]
-mod build_info;
-#[cfg(target_arch = "wasm32")]
-mod compression;
-#[cfg(target_arch = "wasm32")]
-mod execute;
-#[cfg(target_arch = "wasm32")]
-mod foreign_call;
-#[cfg(target_arch = "wasm32")]
-mod js_witness_map;
-#[cfg(target_arch = "wasm32")]
-mod logging;
-#[cfg(target_arch = "wasm32")]
-mod public_witness;
+cfg_if::cfg_if! {
+    if #[cfg(target_arch = "wasm32")] {
+        mod barretenberg;
+        mod build_info;
+        mod compression;
+        mod execute;
+        mod foreign_call;
+        mod js_witness_map;
+        mod logging;
+        mod public_witness;
 
-#[cfg(target_arch = "wasm32")]
-pub use build_info::build_info;
-#[cfg(target_arch = "wasm32")]
-pub use compression::{compress_witness, decompress_witness};
-#[cfg(target_arch = "wasm32")]
-pub use execute::execute_circuit;
-#[cfg(target_arch = "wasm32")]
-pub use js_witness_map::JsWitnessMap;
-#[cfg(target_arch = "wasm32")]
-pub use logging::{init_log_level, LogLevel};
-#[cfg(target_arch = "wasm32")]
-pub use public_witness::{get_public_parameters_witness, get_public_witness, get_return_witness};
+        pub use build_info::build_info;
+        pub use compression::{compress_witness, decompress_witness};
+        pub use execute::execute_circuit;
+        pub use js_witness_map::JsWitnessMap;
+        pub use logging::{init_log_level, LogLevel};
+        pub use public_witness::{get_public_parameters_witness, get_public_witness, get_return_witness};
+
+    }
+}
