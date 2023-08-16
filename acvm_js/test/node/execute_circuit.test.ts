833e4881a5ba8cd1f3ae3390816e43a17e3173ff
dbanks12 <david@aztecprotocol.com>
2023-08-16 18:23:19 +0000

    update execute circuit tests to initialize SimulatedBackend, add test that executes two circuits with same backend

--- acvm_js/test/node/execute_circuit.test.ts
+++ acvm_js/test/node/execute_circuit.test.ts
@@ -1,152 +1,197 @@
 import { expect } from "chai";
 import {
+  createBackend,
   executeCircuit,
+  SimulatedBackend,
   WitnessMap,
   ForeignCallHandler,
 } from "../../../result/";
 
-it("successfully executes circuit and extracts return value", async () => {
-  const { bytecode, initialWitnessMap, resultWitness, expectedResult } =
-    await import("../shared/addition");
-
-  const solvedWitness: WitnessMap = await executeCircuit(
-    bytecode,
-    initialWitnessMap,
-    () => {
-      throw Error("unexpected oracle");
-    }
-  );
-
-  // Solved witness should be consistent with initial witness
-  initialWitnessMap.forEach((value, key) => {
-    expect(solvedWitness.get(key) as string).to.be.eq(value);
+describe("executeCircuit tests", () => {
+  let backend: SimulatedBackend;
+  beforeEach(async () => {
+    backend = await createBackend();
   });
 
-  // Solved witness should contain expected return value
-  expect(solvedWitness.get(resultWitness)).to.be.eq(expectedResult);
-});
+  it("successfully executes circuit and extracts return value", async () => {
+    const { bytecode, initialWitnessMap, resultWitness, expectedResult } =
+      await import("../shared/addition");
 
-it("successfully processes simple brillig foreign call opcodes", async () => {
-  const {
-    bytecode,
-    initialWitnessMap,
-    expectedWitnessMap,
-    oracleResponse,
-    oracleCallName,
-    oracleCallInputs,
-  } = await import("../shared/foreign_call");
+    const solvedWitness: WitnessMap = await executeCircuit(
+      backend,
+      bytecode,
+      initialWitnessMap,
+      () => {
+        throw Error("unexpected oracle");
+      }
+    );
 
-  let observedName = "";
-  let observedInputs: string[][] = [];
-  const foreignCallHandler: ForeignCallHandler = async (
-    name: string,
-    inputs: string[][]
-  ) => {
-    // Throwing inside the oracle callback causes a timeout so we log the observed values
-    // and defer the check against expected values until after the execution is complete.
-    observedName = name;
-    observedInputs = inputs;
+    // Solved witness should be consistent with initial witness
+    initialWitnessMap.forEach((value, key) => {
+      expect(solvedWitness.get(key) as string).to.be.eq(value);
+    });
 
-    return oracleResponse;
-  };
-  const solved_witness: WitnessMap = await executeCircuit(
-    bytecode,
-    initialWitnessMap,
-    foreignCallHandler
-  );
+    // Solved witness should contain expected return value
+    expect(solvedWitness.get(resultWitness)).to.be.eq(expectedResult);
+  });
 
-  // Check that expected values were passed to oracle callback.
-  expect(observedName).to.be.eq(oracleCallName);
-  expect(observedInputs).to.be.deep.eq(oracleCallInputs);
+  it("successfully processes simple brillig foreign call opcodes", async () => {
+    const {
+      bytecode,
+      initialWitnessMap,
+      expectedWitnessMap,
+      oracleResponse,
+      oracleCallName,
+      oracleCallInputs,
+    } = await import("../shared/foreign_call");
 
-  // If incorrect value is written into circuit then execution should halt due to unsatisfied constraint in
-  // arithmetic opcode. Nevertheless, check that returned value was inserted correctly.
-  expect(solved_witness).to.be.deep.eq(expectedWitnessMap);
-});
+    let observedName = "";
+    let observedInputs: string[][] = [];
+    const foreignCallHandler: ForeignCallHandler = async (
+      name: string,
+      inputs: string[][]
+    ) => {
+      // Throwing inside the oracle callback causes a timeout so we log the observed values
+      // and defer the check against expected values until after the execution is complete.
+      observedName = name;
+      observedInputs = inputs;
 
-it("successfully processes complex brillig foreign call opcodes", async () => {
-  const {
-    bytecode,
-    initialWitnessMap,
-    expectedWitnessMap,
-    oracleResponse,
-    oracleCallName,
-    oracleCallInputs,
-  } = await import("../shared/complex_foreign_call");
+      return oracleResponse;
+    };
 
-  let observedName = "";
-  let observedInputs: string[][] = [];
-  const foreignCallHandler: ForeignCallHandler = async (
-    name: string,
-    inputs: string[][]
-  ) => {
-    // Throwing inside the oracle callback causes a timeout so we log the observed values
-    // and defer the check against expected values until after the execution is complete.
-    observedName = name;
-    observedInputs = inputs;
+    const solved_witness: WitnessMap = await executeCircuit(
+      backend,
+      bytecode,
+      initialWitnessMap,
+      foreignCallHandler
+    );
 
-    return oracleResponse;
-  };
-  const solved_witness: WitnessMap = await executeCircuit(
-    bytecode,
-    initialWitnessMap,
-    foreignCallHandler
-  );
+    // Check that expected values were passed to oracle callback.
+    expect(observedName).to.be.eq(oracleCallName);
+    expect(observedInputs).to.be.deep.eq(oracleCallInputs);
 
-  // Check that expected values were passed to oracle callback.
-  expect(observedName).to.be.eq(oracleCallName);
-  expect(observedInputs).to.be.deep.eq(oracleCallInputs);
+    // If incorrect value is written into circuit then execution should halt due to unsatisfied constraint in
+    // arithmetic opcode. Nevertheless, check that returned value was inserted correctly.
+    expect(solved_witness).to.be.deep.eq(expectedWitnessMap);
+  });
 
-  // If incorrect value is written into circuit then execution should halt due to unsatisfied constraint in
-  // arithmetic opcode. Nevertheless, check that returned value was inserted correctly.
-  expect(solved_witness).to.be.deep.eq(expectedWitnessMap);
-});
+  it("successfully processes complex brillig foreign call opcodes", async () => {
+    const {
+      bytecode,
+      initialWitnessMap,
+      expectedWitnessMap,
+      oracleResponse,
+      oracleCallName,
+      oracleCallInputs,
+    } = await import("../shared/complex_foreign_call");
 
-it("successfully executes a Pedersen opcode", async function () {
-  this.timeout(10000);
-  const { bytecode, initialWitnessMap, expectedWitnessMap } = await import(
-    "../shared/pedersen"
-  );
+    let observedName = "";
+    let observedInputs: string[][] = [];
+    const foreignCallHandler: ForeignCallHandler = async (
+      name: string,
+      inputs: string[][]
+    ) => {
+      // Throwing inside the oracle callback causes a timeout so we log the observed values
+      // and defer the check against expected values until after the execution is complete.
+      observedName = name;
+      observedInputs = inputs;
 
-  const solvedWitness: WitnessMap = await executeCircuit(
-    bytecode,
-    initialWitnessMap,
-    () => {
-      throw Error("unexpected oracle");
-    }
-  );
+      return oracleResponse;
+    };
 
-  expect(solvedWitness).to.be.deep.eq(expectedWitnessMap);
-});
+    const solved_witness: WitnessMap = await executeCircuit(
+      backend,
+      bytecode,
+      initialWitnessMap,
+      foreignCallHandler
+    );
 
-it("successfully executes a FixedBaseScalarMul opcode", async () => {
-  const { bytecode, initialWitnessMap, expectedWitnessMap } = await import(
-    "../shared/fixed_base_scalar_mul"
-  );
+    // Check that expected values were passed to oracle callback.
+    expect(observedName).to.be.eq(oracleCallName);
+    expect(observedInputs).to.be.deep.eq(oracleCallInputs);
 
-  const solvedWitness: WitnessMap = await executeCircuit(
-    bytecode,
-    initialWitnessMap,
-    () => {
-      throw Error("unexpected oracle");
-    }
-  );
+    // If incorrect value is written into circuit then execution should halt due to unsatisfied constraint in
+    // arithmetic opcode. Nevertheless, check that returned value was inserted correctly.
+    expect(solved_witness).to.be.deep.eq(expectedWitnessMap);
+  });
 
-  expect(solvedWitness).to.be.deep.eq(expectedWitnessMap);
-});
+  it("successfully executes a Pedersen opcode", async function () {
+    this.timeout(10000);
+    const { bytecode, initialWitnessMap, expectedWitnessMap } = await import(
+      "../shared/pedersen"
+    );
 
-it("successfully executes a SchnorrVerify opcode", async () => {
-  const { bytecode, initialWitnessMap, expectedWitnessMap } = await import(
-    "../shared/schnorr_verify"
-  );
+    const solvedWitness: WitnessMap = await executeCircuit(
+      backend,
+      bytecode,
+      initialWitnessMap,
+      () => {
+        throw Error("unexpected oracle");
+      }
+    );
 
-  const solvedWitness: WitnessMap = await executeCircuit(
-    bytecode,
-    initialWitnessMap,
-    () => {
-      throw Error("unexpected oracle");
-    }
-  );
+    expect(solvedWitness).to.be.deep.eq(expectedWitnessMap);
+  });
 
-  expect(solvedWitness).to.be.deep.eq(expectedWitnessMap);
+  it("successfully executes two circuits with same backend", async function () {
+    this.timeout(10000);
+    const { bytecode, initialWitnessMap, expectedWitnessMap } = await import(
+      "../shared/pedersen"
+    );
+
+    const solvedWitness0: WitnessMap = await executeCircuit(
+      backend,
+      bytecode,
+      initialWitnessMap,
+      () => {
+        throw Error("unexpected oracle");
+      }
+    );
+
+    expect(solvedWitness0).to.be.deep.eq(expectedWitnessMap);
+
+    const solvedWitness1: WitnessMap = await executeCircuit(
+      backend,
+      bytecode,
+      initialWitnessMap,
+      () => {
+        throw Error("unexpected oracle");
+      }
+    );
+    expect(solvedWitness1).to.be.deep.eq(expectedWitnessMap);
+  });
+
+  it("successfully executes a FixedBaseScalarMul opcode", async () => {
+    const { bytecode, initialWitnessMap, expectedWitnessMap } = await import(
+      "../shared/fixed_base_scalar_mul"
+    );
+
+    const solvedWitness: WitnessMap = await executeCircuit(
+      backend,
+      bytecode,
+      initialWitnessMap,
+      () => {
+        throw Error("unexpected oracle");
+      }
+    );
+
+    expect(solvedWitness).to.be.deep.eq(expectedWitnessMap);
+  });
+
+  it("successfully executes a SchnorrVerify opcode", async () => {
+    const { bytecode, initialWitnessMap, expectedWitnessMap } = await import(
+      "../shared/schnorr_verify"
+    );
+
+    const solvedWitness: WitnessMap = await executeCircuit(
+      backend,
+      bytecode,
+      initialWitnessMap,
+      () => {
+        throw Error("unexpected oracle");
+      }
+    );
+
+    expect(solvedWitness).to.be.deep.eq(expectedWitnessMap);
+  });
 });
