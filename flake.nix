4d894c2fa53960d460989dba93ec0355d933163e
Jonathan Bursztyn <jonathan@bursztyn.io>
2023-09-01 14:11:34 +0200

    Remove test from flake, remove comment

--- flake.nix
+++ flake.nix
@@ -145,13 +145,6 @@
           doCheck = true;
         });
 
-        cargo-test-acvm-js = craneLib.cargoTest (acvmjsWasmArgs // {
-          inherit GIT_COMMIT GIT_DIRTY;
-          cargoArtifacts = cargo-artifacts;
-          doCheck = true;
-          cargoExtraArgs = "--package acvm_js -- --test";
-        });
-
         cargo-test = craneLib.cargoTest (commonArgs // sharedEnvironment // {
           inherit GIT_COMMIT GIT_DIRTY;
 
